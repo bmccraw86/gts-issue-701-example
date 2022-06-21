73a7e0aed1414140b9f1b2112ed1618787cd194c
brandon.mccraw <brandon.mccraw@daveramsey.com>
2022-06-21 15:00:49 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+  const signingKey = new RsKmsSigningKey("entitlement-signing-key", RsKmsKeyType.Rsa2048)
+    .WithDescription("The key used by the Entitlements service to sign entitlement tokens.");
+
+  const domainsTable = new RsDynamoDbTable("entmnt-domains", "id")
+    .WithProvisionedBilling(2, 1);
